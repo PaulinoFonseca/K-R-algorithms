9fadee1843ad32d3dfa502ac7f686c1b53694865
paulino <pfvjpoli@usp.br>
2021-08-06 10:05:38 -0300

    LIFO documentation

--- stack.c
+++ stack.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "calc.h"
 #define MAXVAL 100
 
 int sp = 0;
 double val[MAXVAL];
+
+/* the following code implement a stack in C
+    a stack is a data structure that stores elements
+    following the last in, first out directive (LIFO)
+    meaning that the last element pushed in the stack
+    will be the first one to be popped out of it */
 
 /* push: push f onto value stack */
 void push(double f) 
